8f95597e4f7b93d47cd484ec88ca95c5e4b0c8fb
Cristhofer Travieso <cristhofert97@gmail.com>
2014-02-27 14:47:09 -0200

    Add activity directory, setup.py and create a activity
    Signed-off-by: Cristhofer Travieso <cristhofert97@gmail.com>

---
+++ activity.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2012 Cristhofer Travieso <cristhofert97@gmail.com>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from gi.repository import Gtk
+
+class Activity(activity.Activity):
+    def __init__(self, handle):
+        activity.Activity.__init__(self, handle, True)
+
+        #toolbar
+        toolbarbox = ToolbarBox()
+
+        activity_button = ActivityToolbarButton(self)
+        toolbarbox.toolbar.insert(activity_button, 0)
+
+        separator = Gtk.SeparatorToolItem()
+        activity_button.page.insert(separator, -1
+
+        stopbtn = StopButton(self)
+        toolbarbox.toolbar.insert(stopbtn, -1)
+
+        self.set_toolbar_box(toolbarbox)
+        self.show_all()
