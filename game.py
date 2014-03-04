6b5689e6876d01498b5a070d9be679b64d302068
Cristhofer Travieso <cristhofert97@gmail.com>
2014-03-04 15:44:14 -0200

    Add Food class
    Signed-off-by: Cristhofer Travieso <cristhofert97@gmail.com>

--- game.py
+++ game.py
@@ -13,12 +13,17 @@ class Game(glucosa.GameArea):
         _events = glucosa.Events(self)
         _pet = Pet()
         mouth = Mouth()
+        banana = Food('banana.png')
+        
         self.connect('draw', _pet._update)
         self.connect('draw', mouth._update)
         _events.connect('mouse-moved', mouth.move)
+        _events.connect('mouse-moved', banana.move)
+
 
         self.add_sprite(_pet)
         self.add_sprite(mouth)
+        self.add_sprite(banana)
 
 
 class Pet(glucosa.Sprite):
@@ -48,6 +53,25 @@ class Mouth(glucosa.Sprite):
             self.set_image(self._image2)
         else:
             self.set_image(self._image)
+    
+class Food(glucosa.Sprite):
+    def __init__(self, image):
+        _image = glucosa.Image(image)
+        glucosa.Sprite.__init__(self, _image, 400, 100)
+
+        self._to_rotate = 0
+
+    def _update(self, widget, event):
+        pass
+
+    def move(self, widget, data):
+        lr =  data['x'] > self.get_left() and  data['x'] > self.get_left()
+        tb = data['y'] > self.get_top() and data['y'] < self.get_bottom() + 100
+        a = lr and tb
+        if a:
+            self.set_scale(2)
+        else:
+            self.set_scale(1)
 
 if __name__ == '__main__':
     w = gtk.Window()
