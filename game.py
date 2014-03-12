fc97184f16df62d8a45a25e23f2c2214cbb5b071
Cristhofer Travieso <cristhofert97@gmail.com>
2014-03-12 20:38:14 -0300

    Add collision peach with mouth
    Signed-off-by: Cristhofer Travieso <cristhofert97@gmail.com>

--- game.py
+++ game.py
@@ -4,27 +4,37 @@ import glucosa
 import gtk
 import gobject
 
+HUNGER = 0
 
 class Game(glucosa.GameArea):
 
     def __init__(self):
         glucosa.GameArea.__init__(self)
 
+        print 'Loading'
+
         _events = glucosa.Events(self)
         _pet = Pet()
-        mouth = Mouth()
-        banana = Food('banana.png', 0, 0)
+        self.mouth = Mouth()
+        self.peach = Food('banana.png', 0, 0, self.mouth)
         
         self.connect('draw', _pet._update)
-        self.connect('draw', mouth._update)
-        _events.connect('mouse-moved', mouth.move)
-        _events.connect('mouse-moved', banana.move)
-
+        self.connect('draw', self.mouth._update)
+        self.connect('draw', self.update)
+        _events.connect('mouse-moved', self.mouth.move)
+        _events.connect('mouse-moved', self.peach.move)
+        _events.connect('mouse-button-pressed', self.peach.click_pressed)
+        _events.connect('mouse-button-released', self.peach.click_released)
 
         self.add_sprite(_pet)
-        self.add_sprite(mouth)
-        self.add_sprite(banana)
+        self.add_sprite(self.mouth)
+        self.add_sprite(self.peach)
 
+        print 'starting'
+
+    def update(self, widget, data):
+        if self.peach.collision_with(self.mouth):
+            self.peach.kill()
 
 class Pet(glucosa.Sprite):
     def __init__(self):
@@ -55,15 +65,18 @@ class Mouth(glucosa.Sprite):
             self.set_image(self._image)
     
 class Food(glucosa.Sprite):
-    def __init__(self, image, _x, _y):
+    def __init__(self, image, _x, _y, mouth):
+        self.start_pos = _x, _y
         _image = glucosa.Image('images/' + image)
-        glucosa.Sprite.__init__(self, _image, _x, _y)
+        glucosa.Sprite.__init__(self, _image, *self.start_pos)
 
         self._to_rotate = 0
+        self.follow = False
+
 
     def _update(self, widget, event):
         pass
-
+        
     def move(self, widget, data):
         lr =  data['x'] > self.get_left() and  data['x'] > self.get_left()
         tb = data['y'] > self.get_top() and data['y'] < self.get_bottom() + 100
@@ -73,6 +86,20 @@ class Food(glucosa.Sprite):
         else:
             self.set_scale(1)
 
+        if self.follow:
+            self.set_pos(data['x'], data['y'])
+            
+    def click_pressed(self, widget, data):
+        #self.set_pos(data['x'], data['y'])
+        self.follow = True
+
+    def click_released(self, widget, data):
+        #self.set_pos(data['x'], data['y'])
+        self.follow = False
+
+    def kill(self):
+        self.set_pos(0, 0)
+        
 if __name__ == '__main__':
     w = gtk.Window()
     w.maximize()
